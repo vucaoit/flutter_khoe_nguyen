0b52a855f966a1cea9ec26df7cdeff8745c49e61
vucaoit <86138777+vucaoit@users.noreply.github.com>
2024-04-11 19:35:15 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_khoe_nguyen
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_khoe_nguyen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_khoe_nguyen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
